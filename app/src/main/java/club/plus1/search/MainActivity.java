87061adcd223331a743cf814e4f71a9ef22a0699
Lavsexpert <lavrov-sergey@yandex.ru>
2019-02-02 23:55:52 +0300

    Добавлено: меню menu с пунктами Настройки, Поиск, Выход
    При нажатии на каждый пункт показывается Toast с названием пункта

--- app/src/main/java/club/plus1/search/MainActivity.java
+++ app/src/main/java/club/plus1/search/MainActivity.java
@@ -2,6 +2,10 @@ package club.plus1.search;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.itemOptions:
+                Toast.makeText(this, R.string.menu_options, Toast.LENGTH_LONG).show();
+                break;
+            case R.id.itemSearch:
+                Toast.makeText(this, R.string.menu_search, Toast.LENGTH_LONG).show();
+                break;
+            case R.id.itemExit:
+                Toast.makeText(this, R.string.menu_exit, Toast.LENGTH_LONG).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
