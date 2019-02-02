22bbf23463679ea06a071839fb18fd4760d12972
Lavsexpert <lavrov-sergey@yandex.ru>
2019-02-03 01:49:31 +0300

    Добавлено: фрагменты main, options, search
    Фрагменты добавлены и подключены через добавленный класс MainFragment
    Фрагмент fragment_main показывается сразу при загрузке
    Фрагменты fragment_options и fragment_search при нажатии пунктов меню
    При нажатии пункта меню Выход - приложение закрывается

--- app/src/main/java/club/plus1/search/MainActivity.java
+++ app/src/main/java/club/plus1/search/MainActivity.java
@@ -1,5 +1,7 @@
 package club.plus1.search;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,6 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState != null){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainer, getFragment(R.layout.fragment_main))
+                    .commit();
+        }
+    }
+
+    protected Fragment getFragment(int id){
+        Fragment fragment;
+        fragment = getSupportFragmentManager().findFragmentById(id);
+        if (fragment == null ) {
+            fragment = MainFragment.newInstance(id);
+        } else {
+            Toast. makeText ( this , "Не меняем" , Toast. LENGTH_SHORT ).show();
+        }
+        return fragment;
     }
 
     @Override
@@ -27,14 +46,30 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.itemOptions:
                 Toast.makeText(this, R.string.menu_options, Toast.LENGTH_LONG).show();
+                switchFragment(R.layout.fragment_options);
                 break;
             case R.id.itemSearch:
                 Toast.makeText(this, R.string.menu_search, Toast.LENGTH_LONG).show();
+                switchFragment(R.layout.fragment_search);
                 break;
             case R.id.itemExit:
                 Toast.makeText(this, R.string.menu_exit, Toast.LENGTH_LONG).show();
+                finish();
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    void switchFragment(int id) {
+        Fragment fragment;
+        fragment = getSupportFragmentManager().findFragmentById(id);
+        if (fragment == null ) {
+            fragment = MainFragment.newInstance(id);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragmentContainer , fragment)
+                    .commit();
+        } else {
+            Toast.makeText( this, "Не меняем", Toast. LENGTH_SHORT ).show();
+        }
     }
 }
---
+++ app/src/main/java/club/plus1/search/MainFragment.java
@@ -0,0 +1,43 @@
+package club.plus1.search;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+    private static final String FRAGMENT = "FRAGMENT";
+    private int fragmentID;
+
+    public static MainFragment newInstance(int id) {
+        Bundle args = new Bundle();
+        args.putInt(FRAGMENT, id);
+
+        MainFragment fragment = new MainFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        if (args != null ) {
+            fragmentID = args.getInt(FRAGMENT);
+        }
+    }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(fragmentID, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
